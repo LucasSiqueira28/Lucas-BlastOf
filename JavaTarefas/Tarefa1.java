776ba4061e004c0ad1e4b56682351fb26392e24f
Lucas Eduardo <lucas.siqueira@toodoo.com.br>
2021-11-24 10:42:21 -0300

    Tarefa1-Java

---
+++ JavaTarefas/Tarefa1.java
@@ -0,0 +1,35 @@
+package JavaTarefas;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/*
+    Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal, 
+    utilizando as seguintes fórmulas:
+        Para homens: (72.7*h) - 58
+        Para mulheres: (62.1*h) - 44.7
+*/
+public class Tarefa1 {
+    public static void main(String[] args) throws IOException {
+        Scanner entrada = new Scanner(System.in);
+
+        double h, pesoIdeal = 0;
+        char sexo;
+
+        System.out.println("Digite uma altura: ");
+        h = entrada.nextDouble();
+
+        System.out.println("É homem (H) ou mulher (M)? ");
+        sexo = (char) System.in.read();
+
+        if (sexo == 'm') {
+            pesoIdeal = (62.1 * h) - 44.7;
+            System.out.printf("Seu peso ideial é de: %1.2f\n", pesoIdeal);
+        } else if (sexo == 'h') {
+            pesoIdeal = (72.7 * h) - 58;
+            System.out.printf("Seu peso ideial é de: %1.2f\n", pesoIdeal);
+        } else {
+            System.out.println("Digite um sexo válido.");
+        }
+    }
+}
