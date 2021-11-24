e92a46c01a34177b6ee6a1c4fd90c81dbde9897f
LucasSiqueira28 <lucas.siqueira@toodoo.com.br>
2021-11-24 14:37:58 -0300

    Exemplos-Java

---
+++ Android/JavaExemplos/RepeticaoWhile.java
@@ -0,0 +1,11 @@
+public class RepeticaoWhile {
+    public static void main(String[] args) {
+        // while - enquanto
+
+        int contador = 0;
+        while (contador != 10) {
+            System.out.println(contador);
+            contador++;
+        }
+    }
+}
