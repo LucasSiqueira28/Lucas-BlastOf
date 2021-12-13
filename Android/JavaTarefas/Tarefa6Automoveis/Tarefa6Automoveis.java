74304c6a1c5c652590723a8835224905062e515b
LucasSiqueira28 <lucas.siqueira@toodoo.com.br>
2021-12-13 09:47:21 -0300

    Tarefa-Android

--- Android/JavaTarefas/Tarefa6Automoveis/Tarefa6Automoveis.java
+++ Android/JavaTarefas/Tarefa6Automoveis/Tarefa6Automoveis.java
@@ -2,6 +2,11 @@ package Tarefa6Automoveis;
 
 public class Tarefa6Automoveis {
     public static void main(String[] args) throws Exception {
+        Carro carro = new Carro("ABC1234", "Fusca", Carro.GASOLINA, "Azul", (short) 1990);
+        CarroLuxo carroluxo = new CarroLuxo("DEF5678", "Uno de Escada", CarroLuxo.GAS, "Branco", (short) 1820, true,
+                false, true);
 
+        System.out.println("Custo do carro, conforme seu combustivel: " + carro.custoGasolina());
+        System.out.println("Custo do carro de luxo, conforme seu combustivel: " + carroluxo.custoGasolina());
     }
 }
